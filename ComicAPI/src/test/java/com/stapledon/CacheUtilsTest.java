4769c71e7996c70c1a6acfb780a9d92a5d62d84b
Adrian Gilbert <adrian@gilbert.ca>
2019-03-25 21:42:58 -0400

    Figuring out what directory bitbucket launches the tests from

--- ComicAPI/src/test/java/com/stapledon/CacheUtilsTest.java
+++ ComicAPI/src/test/java/com/stapledon/CacheUtilsTest.java
@@ -24,7 +24,7 @@ public class CacheUtilsTest {
         ComicItem item1 = new ComicItem();
         item1.id = 42;
         item1.name = "Fake Comic";
-        item1.description = "Comic for Unit Testss";
+        item1.description = "Comic for Unit Tests";
         item1.oldest = LocalDate.of(1995, 05, 31);
         item1.newest = LocalDate.of(2007, 12, 8);
 
@@ -49,7 +49,7 @@ public class CacheUtilsTest {
         File result = subject.findFirst(comicItem(), Direction.FORWARD);
 
         Assert.assertNotNull(result);
-        Assert.assertTrue(result.getAbsolutePath().contains("FakeComic\\2008\\2008-01-10.png"));
+        Assert.assertTrue(String.format("Checking if '%s' contains '\"FakeComic\\\\2008\\2008-01-10.png\"'", result.getAbsolutePath()), result.getAbsolutePath().contains("FakeComic\\2008\\2008-01-10"));
     }
 
 
@@ -60,7 +60,7 @@ public class CacheUtilsTest {
         File result = subject.findFirst(comicItem(), Direction.BACKWARD);
 
         Assert.assertNotNull(result);
-        Assert.assertTrue(result.getAbsolutePath().contains("FakeComic\\2019\\2019-03-22"));
+        Assert.assertTrue(String.format("Checking if '%s' contains '\"FakeComic\\\\2019\\\\2019-03-22\"'", result.getAbsolutePath()), result.getAbsolutePath().contains("FakeComic\\2019\\2019-03-22"));
     }
 
 
