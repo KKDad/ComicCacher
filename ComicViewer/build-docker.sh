f7d83dfec8106feadebfb2ecfc4b2dcac32097fe
Adrian Gilbert <adrian@gilbert.ca>
2022-09-23 08:51:57 -0400

    Build script to build and push separate UI docker image

---
+++ ComicViewer/build-docker.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# Build comics-ui and push to the stapledon docker registry
+
+set -e
+
+BUILD_TAG=$1
+if [ -z $BUILD_TAG ]; then
+   echo "Build Tag is required"
+   exit
+fi
+
+DOCKER_REGISTRY=registry.stapledon.local
+
+
+docker build -f Dockerfile . --tag kkdad/comics-ui:$BUILD_TAG
+docker tag kkdad/comics-ui:$BUILD_TAG $DOCKER_REGISTRY:5000/kkdad/comic-ui:$BUILD_TAG
+docker push $DOCKER_REGISTRY:5000/kkdad/comic-ui:$BUILD_TAG
