024d6f48f2c5fb99b79fbdbe7f07f5f1e205727a
Adrian Gilbert <agilbert@upgrade.com>
2025-05-09 14:12:22 -0400

    Springboot Updates and cleanup deprecations

--- ComicAPI/src/main/java/org/stapledon/config/SwaggerConfig.java
+++ ComicAPI/src/main/java/org/stapledon/config/SwaggerConfig.java
@@ -1,5 +1,9 @@
 package org.stapledon.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,7 +14,17 @@ public class SwaggerConfig implements WebMvcConfigurer {
     public void addViewControllers(final ViewControllerRegistry registry) {
         registry.addRedirectViewController("/", "/swagger-ui.html");
         // use setStatusCode(HttpStatus.XYZ) for any custom status code if required, e.g. MOVED_PERMANENTLY
-        registry.addRedirectViewController("/swagger-ui", "/swagger-ui.html");
+        registry.addRedirectViewController("/swagger-ui", "/swagger-ui/index.html");
         // any other alias
+    }
+
+    @Bean
+    public OpenAPI comicCacherOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Comic Cacher API")
+                        .description("Web comic downloader and viewer application")
+                        .version("1.2.0")
+                        .license(new License().name("MIT License")));
     }
 }
--- ComicAPI/src/test/java/org/stapledon/api/controller/ComicControllerTest.java
+++ ComicAPI/src/test/java/org/stapledon/api/controller/ComicControllerTest.java
@@ -3,7 +3,7 @@ package org.stapledon.api.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.stapledon.api.service.ComicsService;
@@ -33,13 +33,13 @@ class ComicControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private ComicsService comicsService;
 
-    @MockBean
+    @MockitoBean
     private DailyRunner dailyRunner;
 
-    @MockBean
+    @MockitoBean
     private StartupReconciler startupReconciler;
 
     private final ObjectMapper objectMapper = new ObjectMapper()
