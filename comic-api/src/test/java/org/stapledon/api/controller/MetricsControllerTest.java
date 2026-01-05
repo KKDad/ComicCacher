d273fa824887c8805c41bd059a77c30130156803
Adrian Gilbert <adrian@gilbert.ca>
2026-01-05 10:14:11 -0500

    Fix Tests

--- comic-api/src/test/java/org/stapledon/api/controller/MetricsControllerTest.java
+++ comic-api/src/test/java/org/stapledon/api/controller/MetricsControllerTest.java
@@ -4,13 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import org.springframework.http.HttpStatus;
@@ -38,11 +37,6 @@ class MetricsControllerTest {
 
     @InjectMocks
     private MetricsController metricsController;
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
 
     @Test
     void getStorageMetrics_shouldReturnCacheStats() {
